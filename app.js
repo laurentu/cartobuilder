c4596821b45256c281a4e9fa2e7fe9d185eea950
Laurent ulrich <laurentu@gmail.com>
2015-04-20 14:57:04 +0000

    Intermediate commit

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
+var data = require('./routes/data');
 
 var app = express();
 
